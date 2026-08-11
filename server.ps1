2a306ff60d32e00d02a47b48f95eaf0e1785dfb3
ansh00794 <anshv358@gmail.com>
2026-08-11 18:05:11 +0530

    Add files via upload

---
+++ server.ps1
@@ -0,0 +1,42 @@
+$port = 8080
+$prefix = "http://localhost:$port/"
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add($prefix)
+$listener.Start()
+Write-Host "AURA Local Server listening on $prefix"
+
+$folder = $PSScriptRoot
+
+while ($listener.IsListening) {
+    try {
+        $context = $listener.GetContext()
+        $request = $context.Request
+        $response = $context.Response
+
+        $urlPath = [System.Uri]::UnescapeDataString($request.Url.AbsolutePath)
+        if ($urlPath -eq "/") { $urlPath = "/index.html" }
+        
+        $filePath = Join-Path $folder $urlPath.TrimStart('/')
+
+        if (Test-Path $filePath -PathType Leaf) {
+            $bytes = [System.IO.File]::ReadAllBytes($filePath)
+            $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+            switch ($ext) {
+                ".html" { $response.ContentType = "text/html; charset=utf-8" }
+                ".css"  { $response.ContentType = "text/css; charset=utf-8" }
+                ".js"   { $response.ContentType = "application/javascript; charset=utf-8" }
+                ".png"  { $response.ContentType = "image/png" }
+                ".jpg"  { $response.ContentType = "image/jpeg" }
+                ".jpeg" { $response.ContentType = "image/jpeg" }
+                default { $response.ContentType = "application/octet-stream" }
+            }
+            $response.ContentLength64 = $bytes.Length
+            $response.OutputStream.Write($bytes, 0, $bytes.Length)
+        } else {
+            $response.StatusCode = 404
+        }
+        $response.Close()
+    } catch {
+        # ignore context errors on shutdown
+    }
+}
